78bd3617220caac11c6cc06c1a86b8917e9f7f1c
okaduki <okaduki1@gmail.com>
2018-11-03 22:48:14 +0900

    Add add and sub

--- 9cc.c
+++ 9cc.c
@@ -10,7 +10,25 @@ int main(int argc, char **argv){
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+
+    char* ptr = argv[1];
+    
+    printf("  mov rax, %ld\n", strtol(ptr, &ptr, 10));
+    while(*ptr != '\0'){
+        if(*ptr == '+'){
+            ++ptr;
+            printf("  add rax, %ld\n", strtol(ptr, &ptr, 10));
+        }
+        else if(*ptr == '-'){
+            ++ptr;
+            printf("  sub rax, %ld\n", strtol(ptr, &ptr, 10));
+        }
+        else{
+            fprintf(stderr, "invalid character %c\n", *ptr);
+            return 1;
+        }
+    }
+
     printf("  ret\n");
 
     return 0;
